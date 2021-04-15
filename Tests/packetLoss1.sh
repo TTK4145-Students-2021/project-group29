232df58541b6f6291295b501f1eadfffe1394d4b
guroveglo <guro@veglo.com>
2021-04-15 17:25:57 +0200

    Added correct udp ports

--- Tests/packetLoss1.sh
+++ Tests/packetLoss1.sh
@@ -1,5 +1,5 @@
-sudo iptables -A INPUT -p udp --dport 16000 -m statistic --mode random --probability 0.2 -j DROP
-sudo iptables -A INPUT -p udp --dport 16001 -m statistic --mode random --probability 0.2 -j DROP
+sudo iptables -A INPUT -p udp --dport 42034 -m statistic --mode random --probability 0.4 -j DROP
+sudo iptables -A INPUT -p udp --dport 42035 -m statistic --mode random --probability 0.4 -j DROP
 
 # Add rules by:      chmod +x filename     followed by    ./filename   
 # Delete rules by:   sudo iptables --flush / sudo iptables -F
